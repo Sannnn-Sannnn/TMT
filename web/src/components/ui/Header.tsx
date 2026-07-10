28bc09bcfb6b61aa72b132ce5765d1c3c32dc058
Bang Santiago <sbang@mail.austral.edu.ar>
2026-07-10 00:39:04 -0300

    Working on Dashboard Backup

--- web/src/components/ui/Header.tsx
+++ web/src/components/ui/Header.tsx
@@ -1,7 +1,22 @@
+import { MoonStar, Sun } from "lucide-react";
+
+import { useTheme } from "@/hooks/useTheme.ts";
+
 export default function Header() {
+    const { isDark, toggle } = useTheme();
+
     return (
-        <div className="flex items-center pl-10 h-16 w-screen bg-primary font-heading text-primary-foreground text-4xl shrink-0">
-            TMT
+        <div className="flex items-center justify-between pl-10 pr-10 h-1/14 w-screen bg-primary">
+            <div className={"font-heading text-primary-foreground text-4xl"}>
+                TMT
+            </div>
+            <div onClick={toggle}>
+                {isDark ? (
+                    <MoonStar className={"h-8 w-8 text-primary-foreground"} />
+                ) : (
+                    <Sun className={"h-8 w-8 text-primary-foreground"} />
+                )}
+            </div>
         </div>
     )
 }
--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -1,39 +1,43 @@
 import { Card } from "@/components/ui/card.tsx"
-import { Button } from "@/components/ui/button.tsx"
-import { Input } from "@/components/ui/input.tsx";
-
-import { useTheme } from "@/hooks/useTheme.ts"
+import Header from "@/components/ui/Header.tsx";
 
 export default function Dashboard() {
-    const {isDark, toggle} = useTheme()
-
     return (
-        <div className="flex h-screen w-screen p-5 gap-x-5">
-            <Card className={"flex flex-col flex-1 p-5 gap-y-5"}>
-                <Button onClick={toggle}>
-                    {isDark ? "Modo oscuro" : "Modo claro"}
-                </Button>
-                <Input></Input>
-            </Card>
-            <Card className={"flex flex-col flex-1 p-5 gap-y-5"}>
-                <div className={"flex items-center justify-center p-5 bg-today-dark"}>
-                    <div className={"h-5 w-full bg-today"}/>
-                </div>
-                <div className={"flex gap-x-5"}>
-                    <div className={"w-10 h-10 rounded-full bg-today-dark"}/>
-                    <div className={"w-10 h-10 rounded-full bg-today"}/>
-                    <div className={"w-10 h-10 rounded-full bg-week"}/>
-                    <div className={"w-10 h-10 rounded-full bg-month"}/>
-                    <div className={"w-10 h-10 rounded-full bg-overdue"}/>
-                </div>
-                <div className="flex flex-col gap-4 p-6 max-w-md">
-                    <div className="rounded-lg bg-primary text-primary-foreground p-4">Primary / Primary Foreground</div>
-                    <div className="rounded-lg bg-secondary text-secondary-foreground p-4">Secondary / Secondary Foreground</div>
-                    <div className="rounded-lg bg-muted text-muted-foreground p-4">Muted / Muted Foreground</div>
-                    <div className="rounded-lg bg-accent text-accent-foreground p-4">Accent / Accent Foreground</div>
-                </div>
-            </Card>
-            <Card className={"flex flex-col flex-1 p-5 gap-y-5"} />
+        <div className="flex flex-col h-screen w-screen">
+            <Header />
+            <main className={"flex h-full w-full gap-x-5 p-5"}>
+                { /* Timer */ }
+                <Card className={"flex flex-col w-1/4 p-5 gap-y-5 border"}>
+                </Card>
+
+                { /* Today's tasks, overdue tasks */ }
+                <Card className={"flex flex-col flex-1 p-5 gap-y-5 border"}>
+                    <div className={"flex flex-col p-4 bg-overdue w-1/3 rounded-lg"}>
+                        <p className={"text-3xl font-heading font-medium dark:text-background"}>
+                            Overdue
+                        </p>
+                    </div>
+                    <div className={"flex flex-col p-4 bg-today w-1/3 rounded-lg"}>
+                        <p className={"text-3xl font-heading font-medium dark:text-background"}>
+                            Today
+                        </p>
+                    </div>
+                </Card>
+
+                { /* This week's tasks, this month's tasks */ }
+                <Card className={"flex flex-col flex-1 p-5 gap-y-5 border"}>
+                    <div className={"flex flex-col p-4 bg-week w-1/3 rounded-lg"}>
+                        <p className={"text-3xl font-heading font-medium dark:text-background"}>
+                            This week
+                        </p>
+                    </div>
+                    <div className={"flex flex-col p-4 bg-month w-1/3 rounded-lg"}>
+                        <p className={"text-3xl font-heading font-medium dark:text-background"}>
+                            This Month
+                        </p>
+                    </div>
+                </Card>
+            </main>
         </div>
     )
 }
